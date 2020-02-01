b29d5ccb61eda49c6088e0b8bd19bcd51314cb00
Alexvue <SimCityrulit22@gmail.com>
2020-02-01 15:30:49 +0300

    Initial commit

--- Симулятор РПГ/MainWindow.xaml.cs
+++ Симулятор РПГ/MainWindow.xaml.cs
@@ -12,17 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace Симулятор_РПГ
+namespace RPG
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
     }
 }
