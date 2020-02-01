e544fa5c0f9bd21c3a437917e72a3781d2f22f7d
Alexvue <SimCityrulit22@gmail.com>
2020-02-01 16:43:42 +0300

    Point 1.7

---
+++ Симулятор РПГ/Hero.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    class Hero
+    {
+        public string Name;
+        private int birthYear;
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - birthYear;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    birthYear = DateTime.Now.Year;
+                }else if (value > 100)
+                {
+                    birthYear = DateTime.Now.Year - 100;
+                }
+                else
+                {
+                    birthYear = DateTime.Now.Year - value;
+                }
+            }
+        }
+        private bool? gender;
+        public string Gender
+        {
+            get
+            {
+                if (gender == true)
+                {
+                    return "Мужчинa";
+                }
+                else if (gender == false)
+                {
+                    return "Женщина";
+                }
+                else
+                {
+                    return "Ты вообще кто?";
+                }
+            }
+            set
+            {
+                if (value == "Мужчина")
+                {
+                    gender = true;
+                }else if (value == "Женщина")
+                {
+                    gender = false;
+                }
+                else
+                {
+                    gender = null;
+                }
+            }
+        }
+        private int lvl;
+        public int LVL
+        {
+            get
+            {
+                return lvl;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    lvl = 0;
+                }else if (value > 10)
+                {
+                    lvl = 10;
+                }
+                else
+                {
+                    lvl = value;
+                }
+            }
+        }
+        private int strength;
+        public int Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    strength = 0;
+                }
+                else if (value > 10)
+                {
+                    strength = 10;
+                }
+                else
+                {
+                    strength = value;
+                }
+            }
+        }
+        private int intellect;
+        public int Intellect
+        {
+            get
+            {
+                return intellect;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    intellect = 0;
+                }
+                else if (value > 10)
+                {
+                    intellect = 10;
+                }
+                else
+                {
+                    intellect = value;
+                }
+            }
+        }
+    }
+}
--- Симулятор РПГ/MainWindow.xaml.cs
+++ Симулятор РПГ/MainWindow.xaml.cs
@@ -21,11 +21,78 @@ namespace RPG
     /// </summary>
     public partial class MainWindow : Window
     {
+        Hero hero;
+
+        private void create_hero()
+        {
+            hero = new Hero();
+            hero.Name = tb_sett_name.Text;
+            hero.Age = Int32.Parse(tb_sett_age.Text);
+            if (cb_sett_gender.SelectedIndex == 0)
+            {
+                hero.Gender = "Мужчина";
+            }else if (cb_sett_gender.SelectedIndex == 1)
+            {
+                hero.Gender = "Женщина";
+            }
+            else
+            {
+                hero.Gender = "kekekekekekekek";
+            }
+            hero.LVL = 1;
+            hero.Strength = (int)(sl_strength.Value);
+            hero.Intellect = (int)(sl_intellect.Value);
+        }
+
+        private void show_hero()
+        {
+            tb_name.Text = hero.Name;
+            tb_age.Text = hero.Age.ToString();
+            tb_gender.Text = hero.Gender;
+            tb_general.Text = "Уровень: " + hero.LVL.ToString() + '\n';
+            tb_abilities.Text = "Сила: " + hero.Strength.ToString() + '\n';
+            tb_abilities.Text += "Интеллект: " + hero.Intellect.ToString();
+        }
 
         public MainWindow()
         {
             InitializeComponent();
+            tab_control.SelectedIndex = 1;
         }
 
+        private void Btn_apply_settings_Click(object sender, RoutedEventArgs e)
+        {
+            create_hero();
+
+            show_hero();
+
+            tab_control.SelectedIndex = 0;
+        }
+
+        private void Tab_control_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (tab_control.SelectedIndex == 1)
+            {
+                tb_sett_name.Text = hero.Name;
+                tb_sett_age.Text = hero.Age.ToString();
+                if (hero.Gender == "Мужчина")
+                {
+                    cb_sett_gender.SelectedIndex = 0;
+                }else if (hero.Gender == "Женщина")
+                {
+                    cb_sett_gender.SelectedIndex = 1;
+                }
+                else
+                {
+                    cb_sett_gender.SelectedIndex = 2;
+                }
+                sl_strength.Value = (double)hero.Strength;
+                sl_intellect.Value = (double)hero.Strength;
+            }
+            else
+            {
+                show_hero();
+            }
+        }
     }
 }
